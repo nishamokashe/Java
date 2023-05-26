1cd476adb7c7a3e967f4dd6bde277d5883b263f3
nishamokashe <nishamokase18@gmail.com>
2023-05-26 08:56:46 +0530

    demo.java
    My First program how to write java program. (demo.java)

---
+++ demo.java
@@ -0,0 +1,9 @@
+
+//WAP to understand how to write java program
+class Demo
+{
+     public static void main(String[] args) 
+    {
+        System.out.println("\n\n Welcome in Java Programning!");
+    }
+}
