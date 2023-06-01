2e9a883ecd9f270dc726746860a2b615a12a6c72
nishamokashe <nishamokase18@gmail.com>
2023-06-01 08:43:26 +0530

    Circumference.java
    Basic programs, Circumference.java

---
+++ Circumference.java
@@ -0,0 +1,11 @@
+
+//WAP to calculate the area of Circumference 
+class Circumference
+{
+     public static void main(String[] args) 
+    {
+        double pi=3.14, r=44, area;
+        area= (2*pi*r);
+        System.out.println("\n\n Area of Circumference is"+area);
+    }
+}
