0b792fa102f18359def07348342622c72c7d516f
nishamokashe <nishamokase18@gmail.com>
2023-06-01 08:41:01 +0530

    Cylender.java
    Basic programs, Square.java

---
+++ Cylender.java
@@ -0,0 +1,12 @@
+
+//WAP to calculate the area of Cylender 
+class Cylender
+{
+     public static void main(String[] args) 
+    {
+        // Area of Cylender (Area= 2*PI*radius*Heidth)
+        double pi=3.14, r=4.5, h=2.2, area;
+        area= (2*pi*r*h);
+        System.out.println("\n\n Area of Cylender is"+area);
+    }
+}
