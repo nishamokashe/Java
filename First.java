66f287a547a2f6c4b3746f7c1100e87eecae1760
nishamokashe <nishamokase18@gmail.com>
2023-06-01 08:41:48 +0530

    First.java
    Basic programs, First.java

---
+++ First.java
@@ -0,0 +1,12 @@
+
+//WAP to understand how to write java program
+//WAP to calculate the area of circle 
+class First
+{
+     public static void main(String[] args) 
+    {
+        double pi=3.14, r=2.5, area;
+        area= (pi*r*r);
+        System.out.println("\n\n Area of Circle is"+area);
+    }
+}
